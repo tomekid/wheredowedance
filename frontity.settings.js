4f265776edda69d7fb72fb5ba01e0e13531ec768
tomekid <thomaskiddle0@gmail.com>
2020-05-20 21:44:37 +1200

    first upload

---
+++ frontity.settings.js
@@ -0,0 +1,54 @@
+const settings = {
+  "name": "wheredoweance",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.io",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/Homepage"
+            ],
+            [
+              "Events",
+              "/events/"
+            ],
+            [
+              "Resources & Publications",
+              "/resources-publications/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ] /*,
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }*/
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://public-api.wordpress.com/wp/v2/sites/wheredowedance.wordpress.com",
+          "Homepage": "/homepage",
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
